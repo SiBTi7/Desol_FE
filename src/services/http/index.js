31d3c0b227914463939849e2232bb04c7e585438
subtain-ahmed <sibtiataj@gmail.com>
2024-12-16 03:23:20 +0500

    first commit

--- src/services/http/index.js
+++ src/services/http/index.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000',
-    timeout: 10000,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: "https://desol-be-eta.vercel.app",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // axiosInstance.interceptors.request.use(function (config) {
